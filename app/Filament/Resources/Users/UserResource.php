2b7ab19b256094e523974246ed90d7472a064203
AlfredaDhaifullah04= <mahezzeham@gmail.com=>
2026-04-18 17:21:38 +0700

    membuat model & migration

---
+++ app/Filament/Resources/Users/UserResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\Users;
+
+use App\Filament\Resources\Users\Pages\CreateUser;
+use App\Filament\Resources\Users\Pages\EditUser;
+use App\Filament\Resources\Users\Pages\ListUsers;
+use App\Filament\Resources\Users\Schemas\UserForm;
+use App\Filament\Resources\Users\Tables\UsersTable;
+use App\Models\User;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::UserGroup;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return UserForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+
+        $table = UsersTable::configure($table);
+        $table = $table->columns(array_merge(
+            $table->getColumns(),
+            [
+                \Filament\Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime('d M Y H:i'),
+            ]
+        ));
+        return $table;
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListUsers::route('/'),
+            'create' => CreateUser::route('/create'),
+            'edit' => EditUser::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2026_04_09_000001_update_category_id_foreign_on_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Ubah kolom category_id menjadi unsignedBigInteger jika belum
+            $table->unsignedBigInteger('category_id')->change();
+            // Tambahkan foreign key constraint
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->integer('category_id')->change();
+        });
+    }
+};
